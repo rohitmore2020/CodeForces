cbd44a7a95e7f7e601b6b441e7400ee1e6aa758c
rohitmore2020 <2023bit056@sggs.ac.in>
2026-06-07 16:31:05 +0530

    1837A

---
+++ GrasshopperonaLine.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n , k;
+        cin >> n ; 
+        cin >> k;
+
+        if(n % k != 0 ){
+            cout << "1" << endl;
+            cout << n << endl;
+        }
+        else{
+            cout << "2" << endl;
+            cout << n-1 << " 1" << endl;
+        }
+    }
+
+    return 0;
+}
